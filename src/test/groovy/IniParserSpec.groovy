c4d28f1c0663f104cb6614e7152ee657b89eba69
Balachandran Sivakumar <balachandran@balachandran.org>
2015-07-07 23:43:34 +0530

    Initial commit

---
+++ src/test/groovy/IniParserSpec.groovy
@@ -0,0 +1,20 @@
+import spock.lang.*;
+import org.benignbala.iniparser.IniParser;
+
+class IniParserSpec extends spock.lang.Specification {
+  def ini = new IniParser("/tmp/test.ini")
+
+  def "Check constructor loads the config"() {
+    when:
+    def config = ini.getConfig()
+    then:
+    config['top'] != null
+    config['next'] != null
+  }
+
+  def "Check all sections are loaded"() {
+    when:
+    def config = ini.getAllSections()
+    then:
+    config.size() == 2
+    
